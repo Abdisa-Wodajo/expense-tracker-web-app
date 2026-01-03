09cd6e6e407627b545861f8e842321402237a002
Abdisa-Wodajo <abdisawodajo41@gmail.com>
2026-01-03 09:43:29 -0800

    login page is added

--- frontend/expense-tracker-web-application/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker-web-application/src/pages/Auth/Login.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
+import {useNavigate, Link} from 'react-router-dom'
+import Input from '../../components/inputs/Input';
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword]= useState("");
+  const [error, setError]= useState(null);
+
+  const navigate =useNavigate();
+
+  //handle login form submit
+  const handleLogin = async(e)=>{
+     e.preventDefault();
+  }
   return (
     <AuthLayout>
       <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
       <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
       <p className='text-xs text-slate-700 mt-[5px] mb-6'>Please enter details to log in </p>
 
+     <form onSubmit={handleLogin}>
+      <Input
+  value={email}
+  onchange={(e) => setEmail(e.target.value)}
+  label="Email Address"
+  placeholder="abdisa@example.com"
+  type="text"
+/>
 
+     <Input
+  value={password}
+  onchange={(e) => setPassword(e.target.value)}
+  label="Password"
+  placeholder="Min 8 Character"
+  type="password"
+/>
+
+
+      {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+      <button type='submit' className='btn-primary'>
+        LOGIN
+      </button>
+      <p className='text-[13px] text-slate-800 mt-3'>
+  Don't have an account?{" "}
+  <Link className='font-medium text-primary underline' to='/signup'>
+    SignUp
+  </Link>
+</p>
+      
+
+     </form>
       </div>
     </AuthLayout>
   )
