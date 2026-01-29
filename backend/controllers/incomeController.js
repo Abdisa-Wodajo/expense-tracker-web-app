db2e8ce74c68265749dd2eae968281619dcadace
Abdisa-Wodajo <abdisawodajo41@gmail.com>
2026-01-29 04:28:22 -0800

    Income and Expense api is tested

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const xlsx = require('xlsx');
 const Income = require("../models/Income");
 
 //Add Income Source
@@ -9,9 +9,9 @@ exports.addIncome = async (req, res) => {
         const {icon, source, amount, date} = req.body;
 
         //Validation check for missing fields
-        if(!source || !amount || !date) {
-            return res.status.json({message: "All fields are required"});
-        }
+       if(!source || !amount || !date) {
+    return res.status(400).json({message: "All fields are required"});
+}
 
         const newIncome = new Income({
             userId,
@@ -28,11 +28,50 @@ exports.addIncome = async (req, res) => {
 }
 
 //Get All Income Source
-exports.getAllIncome = async (req, res) => {}
+exports.getAllIncome = async (req, res) => {
+     const userId = req.user.id;
+
+    try{
+        const income = await Income.find({userId}).sort({date: -1});
+        res.json(income);
+    } catch (error) {
+        res.status(500).json({message:"Server Error"});
+    }
+};
 
 //Delete Income Source
-exports.deleteIncome = async (req, res) => {}
+exports.deleteIncome = async (req, res) => {
+    try {
+        await Income.findByIdAndDelete(req.params.id);
+        res.json({message: "Income deleted succesfully"});
+    } catch (error) {
+        res.status(500).json({message: "Server Error"});
+    }
+};
 
 // Download Excel
-exports.downloadIncomeExcel = async (req, res) => {}
+exports.downloadIncomeExcel = async (req, res) => {
+    const userId = req.user.id;
+    try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
+
+    // Prepare data for Excel
+    const data = income.map((item) => ({
+    Source: item.source,
+    Amount: item.amount,
+    Date: item.date,
+    }));
+
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(data);
+    xlsx.utils.book_append_sheet(wb, ws, "Income");
+    xlsx.writeFile(wb, 'income_details.xlsx');
+    res.download('income_details.xlsx');
+
+ } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+    }
+};
+
+
 
